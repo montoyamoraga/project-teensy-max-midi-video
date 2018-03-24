a7d4f369f986a89d269070d4a6e5313ba4b4dd7d
montoyamoraga <montoyamoraga@gmail.com>
2018-03-24 16:18:01 -0400

    add teensy code

--- teensy_midi_cc/teensy_midi_cc.ino
+++ teensy_midi_cc/teensy_midi_cc.ino
@@ -1,27 +1,87 @@
-//teensy send midi control change message
+//teensy send midi
+//control change message
+//by aaron montoya-moraga
+//march 2018
+//running on teensy 3.2
 
-int sendControl = 9;
-int sendValue = 120;
+//using examples found here
+//http://www.instructables.com/id/Max-Sonar-EZ0/
+
+//pin  9 is the max sonar ez0 sensor
+//pin 10 is output to LEDs
+
+//we will be using the pulse width mode of the sensor
+//the scale factor is 147 us per inch
+
+//variables for midi cc communications
+// sendControl needs to CHANGE for each teensy, 1  or 2 or 3
+int sendControl = 1;
 int sendChannel = 1;
+int sendValue = 0;
+
+//variables for sensor and LED
+int sensorPin = 9;
+bool yesPeople = false;
+int yesValue = 127;
+int noValue = 0;
+int thresholdValue = 10;
+int measures[] = {0, 0, 0, 0, 0};
+int measuresSize = 5;
+long measure;
+//int delayTime = 1;
+int exactValue;
 
 void setup() {
- 
   Serial.begin(115200);
-  usbMIDI.setHandlePitchChange(OnPitchChange);
+  analogWriteFrequency(10, 5000);
+  pinMode(sensorPin, INPUT);
 }
 
 void loop() {
+
+  //detect people
+  detectPeople();
+
+  //decide the value to be sent
+  if (yesPeople) {
+    sendValue = yesValue;
+  }
+  else {
+    sendValue = noValue;
+  }
+
+  //send the value over midi cc
+  usbMIDI.sendControlChange(sendControl, sendValue, sendChannel);
+
+}
+
+void detectPeople() {
   
-  //usbMIDI.sendControlChange(control, value, channel);
-  usbMIDI.sendControlChange(sendControl, int(random(127)), sendChannel);
+  //read from the sensor
+  for (int i = 0; i < measuresSize; i++) {
+    //read from the sensor
+    measure = pulseIn(sensorPin, HIGH);
+    //scale and save to the array
+    measures[i] = measure / 58;
+    //wait for stabilization
+    //    delay(delayTime);
+  }
 
-  delay(1000);
+  //average measurements
+  int average = 0;
+  for (int i = 0; i < measuresSize; i++) {
+    average = average + measures[i];
+  }
+  average = average / measuresSize;
+
+  Serial.println(average);
+
+  // check average against threshold
+  if (average < thresholdValue) {
+    yesPeople = true;
+  } else {
+    yesPeople = false;
+  }
+
 }
 
-void OnPitchChange(byte channel, int pitch) {
-  Serial.print("Pitch Change, ch=");
-  Serial.print(channel, DEC);
-  Serial.print(", pitch=");
-  Serial.print(pitch, DEC);
-  Serial.println();
-}
