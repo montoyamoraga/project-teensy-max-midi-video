7d8ff3c80e0682b331abe29bc30dc6f8d9261535
montoyamoraga <montoyamoraga@gmail.com>
2018-03-24 16:57:45 -0400

    add teensy code

--- teensy_midi_cc/teensy_midi_cc.ino
+++ teensy_midi_cc/teensy_midi_cc.ino
@@ -21,20 +21,25 @@ int sendValue = 0;
 
 //variables for sensor and LED
 int sensorPin = 9;
-bool yesPeople = false;
-int yesValue = 127;
-int noValue = 0;
-int thresholdValue = 10;
-int measures[] = {0, 0, 0, 0, 0};
-int measuresSize = 5;
+int ledPin = 10;
+bool isPeople = false;
+int yesPeople = 127;
+int noPeople = 0;
+int onLight = 70;
+int offLight = 4;
+int currentLight = offLight;
+int deltaLight = 1;
+int thresholdValue = 400;
+int measures[] = {0, 0, 0};
+int measuresSize = 3;
 long measure;
-//int delayTime = 1;
-int exactValue;
+long sensorValue;
 
 void setup() {
   Serial.begin(115200);
   analogWriteFrequency(10, 5000);
   pinMode(sensorPin, INPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
@@ -43,45 +48,72 @@ void loop() {
   detectPeople();
 
   //decide the value to be sent
-  if (yesPeople) {
-    sendValue = yesValue;
+  if (isPeople) {
+    sendValue = yesPeople;
   }
   else {
-    sendValue = noValue;
+    sendValue = noPeople;
   }
 
   //send the value over midi cc
   usbMIDI.sendControlChange(sendControl, sendValue, sendChannel);
 
+  //control the led lights
+  ledControl();
+
 }
 
 void detectPeople() {
-  
+
   //read from the sensor
-  for (int i = 0; i < measuresSize; i++) {
-    //read from the sensor
-    measure = pulseIn(sensorPin, HIGH);
-    //scale and save to the array
-    measures[i] = measure / 58;
-    //wait for stabilization
-    //    delay(delayTime);
-  }
+  //  for (int i = 0; i < measuresSize; i++) {
+  //    //read from the sensor
+  //    measure = pulseIn(sensorPin, HIGH);
+  //    //scale and save to the array
+  //    measures[i] = measure / 58;
+  //    //wait for stabilization
+  //    //    delay(delayTime);
+  //  }
+
+  sensorValue = pulseIn(sensorPin, HIGH);
+  Serial.println(sensorValue);
+
 
   //average measurements
-  int average = 0;
-  for (int i = 0; i < measuresSize; i++) {
-    average = average + measures[i];
-  }
-  average = average / measuresSize;
+  //  int average = 0;
+  //  for (int i = 0; i < measuresSize; i++) {
+  //    average = average + measures[i];
+  //  }
+  //  average = average / measuresSize;
 
-  Serial.println(average);
+  //Serial.println(average);
 
   // check average against threshold
-  if (average < thresholdValue) {
-    yesPeople = true;
+  //  if (average < thresholdValue) {
+  if (sensorValue < (long)thresholdValue) {
+    isPeople = true;
   } else {
-    yesPeople = false;
+    isPeople = false;
   }
+
+}
+
+//function for turning on led lights
+void ledControl() {
+
+  //send the value to the led lights
+  analogWrite(ledPin, currentLight);
+
+  //turn up if there is nobody, turn down if there is somebody
+  if (isPeople) {
+    currentLight = currentLight - deltaLight;
+  }
+  else {
+    currentLight = currentLight + deltaLight;
+  }
+
+  //constrain to the maximum and minimum values
+  currentLight = constrain(currentLight, offLight, onLight);
 
 }
 
